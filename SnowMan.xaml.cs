a7f5a7cafdca6b10537ac153063bd5d2d1f4aa3f
Zhan-Gabriel Gerke <zangerke3@gmail.com>
2025-09-18 19:13:44 +0300

    night mode, textToSpeech

--- SnowMan.xaml.cs
+++ SnowMan.xaml.cs
@@ -2,39 +2,47 @@
 using Microsoft.Maui.Controls;
 using Microsoft.Maui.Controls.Shapes;
 using Microsoft.Maui.Layouts;
+using Microsoft.Maui.Media;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace AnExample;
 
 public partial class SnowMan : ContentPage
 {
-    string SelectedItem;
-    Picker picker;
-    Slider slider;
-    Stepper stepper;
-    AbsoluteLayout al;
-    Frame head, bucketBody, handle, firstRing, secondRing, eye1, eye2, carrot, handL, handR;
-    Button action;
-    List<Frame> elements;
-    Random rnd;
+    private volatile uint speed = 2000;
+    private float opacityForFrames = 1.0F;
+    private string SelectedItem;
+    private Picker picker;
+    private Slider slider;
+    private Stepper stepper;
+    private AbsoluteLayout al;
+    private Frame head, bucketBody, handle, firstRing, secondRing, eye1, eye2, carrot, handL, handR;
+    private Label speedLabel;
+    private Button action;
+    private List<Frame> elements;
+    private Random rnd;
+    private bool isNight = false;
+    private Switch isNightSwith;
     public SnowMan()
     {
         InitializeComponent();
         CrossMediaManager.Current.Init();
         rnd = new Random();
-
         handL = new Frame
         {
             BackgroundColor = Colors.Black,
             CornerRadius = 10,
             HasShadow = false,
-            Padding = 0
+            Padding = 0,
+            Opacity = opacityForFrames
         };
         handR = new Frame
         {
             BackgroundColor = Colors.Black,
             CornerRadius = 10,
             HasShadow = false,
-            Padding = 0
+            Padding = 0,
+            Opacity = opacityForFrames
         };
 
         carrot = new Frame
@@ -42,7 +50,8 @@ public partial class SnowMan : ContentPage
             BackgroundColor = Colors.Orange,
             CornerRadius = 10,
             HasShadow = false,
-            Padding = 20
+            Padding = 20,
+            Opacity = opacityForFrames
         };
 
         eye1 = new Frame
@@ -50,7 +59,8 @@ public partial class SnowMan : ContentPage
             BackgroundColor = Colors.Green,
             CornerRadius = 100,
             HasShadow = false,
-            Padding = 20
+            Padding = 20,
+            Opacity = opacityForFrames
 
         };
 
@@ -59,7 +69,8 @@ public partial class SnowMan : ContentPage
             BackgroundColor = Colors.Green,
             CornerRadius = 100,
             HasShadow = false,
-            Padding = 20
+            Padding = 20,
+            Opacity = opacityForFrames
 
         };
 
@@ -68,7 +79,8 @@ public partial class SnowMan : ContentPage
             BackgroundColor = Colors.LightGray,
             CornerRadius = 100,
             HasShadow = false,
-            Padding = new Thickness(10, 5)
+            Padding = new Thickness(10, 5),
+            Opacity = opacityForFrames
         };
 
         bucketBody = new Frame
@@ -77,7 +89,8 @@ public partial class SnowMan : ContentPage
             BorderColor = Colors.Black,
             HasShadow = false,
             CornerRadius = 10,
-            Padding = 0
+            Padding = 0,
+            Opacity = opacityForFrames
         };
 
         handle = new Frame
@@ -85,7 +98,8 @@ public partial class SnowMan : ContentPage
             BackgroundColor = Colors.DarkGray,
             CornerRadius = 5,
             HasShadow = false,
-            Padding = 0
+            Padding = 0,
+            Opacity = opacityForFrames
         };
 
         firstRing = new Frame
@@ -93,7 +107,8 @@ public partial class SnowMan : ContentPage
             BackgroundColor = Colors.LightGray,
             CornerRadius = 100,
             HasShadow = false,
-            Padding = new Thickness(10, 5)
+            Padding = new Thickness(10, 5),
+            Opacity = opacityForFrames
         };
 
 
@@ -102,7 +117,28 @@ public partial class SnowMan : ContentPage
             BackgroundColor = Colors.LightGray,
             CornerRadius = 100,
             HasShadow = false,
-            Padding = new Thickness(10, 5)
+            Padding = new Thickness(10, 5),
+            Opacity = opacityForFrames
+        };
+
+        speedLabel = new Label
+        {
+            BackgroundColor = Colors.LightGray,
+            Padding = new Thickness(10, 5),
+            TextColor = Colors.Black,
+            FontSize = 18,
+            Text = speed.ToString()
+        };
+
+        isNightSwith = new Switch
+        {
+            IsToggled = false
+        };
+
+        isNightSwith.Toggled += (s, e) =>
+        {
+            bool isNight = e.Value; // true if switch is ON
+            this.BackgroundImageSource = isNight ? "winter_street.jpg" : "winter_street_day";
         };
 
         picker = new Picker
@@ -128,23 +164,39 @@ public partial class SnowMan : ContentPage
         {
             Minimum = 0,
             Maximum = 100,
-            Value = 50,
+            Value = 100,
             BackgroundColor = Color.FromRgb(100, 100, 100),
             ThumbColor = Colors.Gray,
             MinimumTrackColor = Colors.White,
             MaximumTrackColor = Colors.Black
         };
 
+        slider.ValueChanged += (s, e) =>
+        {
+            opacityForFrames = (float)e.NewValue / 100;
+            foreach (var el in elements)
+            {
+                el.Opacity = opacityForFrames;
+            }
+        };
+
         stepper = new Stepper
         {
-            Minimum = 0,
-            Maximum = 100,
-            Increment = 1,
-            Value = 20,
+            Minimum = 1000,
+            Maximum = 10000,
+            Increment = 1000,
+            Value = 2000,
             BackgroundColor = Color.FromRgb(100, 100, 100),
             HorizontalOptions = LayoutOptions.Center
         };
-               action = new Button
+
+        stepper.ValueChanged += (s, e) =>
+        {
+            speed = (uint)e.NewValue;
+            speedLabel.Text = speed.ToString();
+        };
+
+        action = new Button
         {
             Text = "Do action",
             FontSize = 12,
@@ -153,6 +205,63 @@ public partial class SnowMan : ContentPage
             FontFamily = "Lucida-400"
         };
         action.Clicked += Btn_Clicked;
+
+        //var shapeList = new View[]
+        //{
+        //    speedLabel, carrot, handR,
+        //    handL, eye1, eye2,
+        //    action, head, bucketBody,
+        //    handle, firstRing, secondRing,
+        //    picker, slider, stepper
+        //};
+        //var x = new double[]
+        //{
+        //    0.98, 0.52, 0.25, // speedLabel, carrot, handR
+        //    0.75, 0.6, 0.45,  // handL, eye1, eye2
+        //    0.9, 0.5, 0.5,    // action, head, bucketBody
+        //    0.5, 0.5, 0.5,    // handle, firstRing, secondRing
+        //    0.1, 0, 0.98      // picker, slider, stepper
+        //};
+
+        //var y = new double[]
+        //{
+        //    0.92, 0.48, 0.6, // speedLabel, carrot, handR
+        //    0.6, 0.48, 0.48, // handL, eye1, eye2
+        //    0.05, 0.48, 0.40, // action, head, bucketBody
+        //    0.45, 0.65, 0.9,  // handle, firstRing, secondRing
+        //    0.05, 1, 1        // picker, slider, stepper
+        //};
+
+        //var scale = new int[]
+        //{
+        //    75, 5, 65,   // speedLabel, carrot, handR
+        //    65, 10, 10,  // handL, eye1, eye2
+        //    100, 100, 100, // action, head, bucketBody
+        //    60, 150, 200, // handle, firstRing, secondRing
+        //    200, 300, 100 // picker, slider, stepper
+        //};
+
+        //var scaleX = new int[]
+        //{
+        //    40, 40, 8,// speedLabel, carrot, handR
+        //    8, 10, 10,// handL, eye1, eye2
+        //    50, 100, 80,// action, head, bucketBody
+        //    10, 150, 200,// handle, firstRing, secondRing
+        //    50, 50,  50//picker, slider, stepper
+        //};
+
+        //for (int i = 0; i < shapeList.Length - 1; i++)
+        //{
+        //    AbsoluteLayout.SetLayoutBounds(shapeList[i], new Rect(x[i], y[i], scale[i], scaleX[i]));
+        //    AbsoluteLayout.SetLayoutFlags(shapeList[i], AbsoluteLayoutFlags.PositionProportional);
+        //    Console.WriteLine(i);
+        //}
+
+        AbsoluteLayout.SetLayoutBounds(isNightSwith, new Rect(0.05, 0.92, 40, 40));
+        AbsoluteLayout.SetLayoutFlags(isNightSwith, AbsoluteLayoutFlags.PositionProportional);
+
+        AbsoluteLayout.SetLayoutBounds(speedLabel, new Rect(0.98, 0.92, 75, 40));
+        AbsoluteLayout.SetLayoutFlags(speedLabel, AbsoluteLayoutFlags.PositionProportional);
 
         AbsoluteLayout.SetLayoutBounds(carrot, new Rect(0.52, 0.48, 5, 40));
         AbsoluteLayout.SetLayoutFlags(carrot, AbsoluteLayoutFlags.PositionProportional);
@@ -193,7 +302,7 @@ public partial class SnowMan : ContentPage
         AbsoluteLayout.SetLayoutBounds(slider, new Rect(0, 1, 300, 50));
         AbsoluteLayout.SetLayoutFlags(slider, AbsoluteLayoutFlags.PositionProportional);
 
-        AbsoluteLayout.SetLayoutBounds(stepper, new Rect(0.98, 1 ,100, 50));
+        AbsoluteLayout.SetLayoutBounds(stepper, new Rect(0.98, 1, 100, 50));
         AbsoluteLayout.SetLayoutFlags(stepper, AbsoluteLayoutFlags.PositionProportional);
 
         elements = new List<Frame> { head, firstRing, secondRing, bucketBody,
@@ -203,8 +312,8 @@ public partial class SnowMan : ContentPage
         {
             Children =
             {
-                head, firstRing, secondRing, bucketBody, handle, picker, 
-                slider, stepper, action, carrot, eye1, eye2, handL, handR
+                head, firstRing, secondRing, bucketBody, handle, picker, isNightSwith,
+                slider, stepper, action, carrot, eye1, eye2, handL, handR, speedLabel
             }
         };
         Content = al;
@@ -214,8 +323,8 @@ public partial class SnowMan : ContentPage
     {
         for (int i = 0; i < elements.Count; i++)
         {
-            elements[i].ScaleTo(0.1, 2000);
-            elements[i].FadeTo(0, 2000);
+            elements[i].ScaleTo(0.1, speed);
+            elements[i].FadeTo(0, speed);
         }
     }
     
@@ -259,6 +368,7 @@ public partial class SnowMan : ContentPage
     private async void Btn_Clicked(object? sender, EventArgs e)
     {
         Button butn = (Button)sender;
+        sound();
         switch (SelectedItem)
         {
             case "Hide":
@@ -296,6 +406,25 @@ public partial class SnowMan : ContentPage
             default:
                 action.Text = "Chose";
                 break;
+        }
+    }
+    public async void sound()
+    {
+        IEnumerable<Locale> locales = await TextToSpeech.Default.GetLocalesAsync();
+        SpeechOptions optionsTextToSpeech = new SpeechOptions()
+        {
+            Pitch = 1.5f,   // 0.0 - 2.0
+            Volume = 0.75f, // 0.0 - 1.0
+            Locale = locales.FirstOrDefault(l => l.Language == "et-EE")
+        };
+
+        try
+        {
+            await TextToSpeech.SpeakAsync("Christmas is coming", optionsTextToSpeech);
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("TTS viga", ex.Message, "OK");
         }
     }
 }
